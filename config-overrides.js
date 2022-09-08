48752bed6e397aeddf86b8b55c02eaca4249be4d
fridolinf <frelly130@gmail.com>
2022-09-09 01:39:44 +0800

    up

--- config-overrides.js
+++ config-overrides.js
@@ -4,8 +4,7 @@ module.exports = function override(config) {
   const fallback = config.resolve.fallback || {};
   Object.assign(fallback, {
     crypto: require.resolve("crypto-browserify"),
-    // stream: require.resolve("stream-browserify"),
-    stream: false,
+    stream: require.resolve("stream-browserify"),
     assert: require.resolve("assert"),
     http: require.resolve("stream-http"),
     https: require.resolve("https-browserify"),
